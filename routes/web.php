6158d739a916c11bd72b3d9dc6d8e2e7528ba77b
Marlon Basten <mbasten@byte5.de>
2021-11-30 13:32:49 +0100

    feat: added home

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('index');
 
 // Route::get('/hallo', 'PostController@hallo');
 Route::get('/hallo', [PostController::class, 'hallo']);
