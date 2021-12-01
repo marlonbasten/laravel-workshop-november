34876f8c84ed15c39a47bc8ab85d95cfc602d2c2
Marlon Basten <mbasten@byte5.de>
2021-12-01 09:04:36 +0100

    feat: Added livewire

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,8 @@
     <head>
         <title>Blog</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        @livewireStyles
     </head>
     <body>
 
@@ -16,7 +18,7 @@
                   <a class="nav-link" href="{{ route('index') }}">Home <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="{{ route('post.index') }}">Posts</a>
+                  <a class="nav-link test" href="{{ route('post.index') }}">Posts</a>
                 </li>
               </ul>
             </div>
@@ -26,5 +28,7 @@
 
         <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+        <script src="{{ asset('js/app.js') }}"></script>
+        @livewireScripts
     </body>
 </html>
---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,10 @@
+<div>
+    {{ $count }}
+    <button wire:click="plus()">Plus</button>
+
+    <br><br><br>
+
+    {{ $name }}
+    <br>
+    <input type="text" wire:model="name">
+</div>
