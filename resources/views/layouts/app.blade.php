81b0bd3663f41fdd2a98ba10599ee74c48d633a5
Marlon Basten <mbasten@byte5.de>
2021-12-01 21:24:35 +0100

    feat: added docker

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,8 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    @livewireStyles
 </head>
 <body>
     <div id="app">
@@ -81,5 +83,6 @@
             @yield('content')
         </main>
     </div>
+    @livewireScripts
 </body>
 </html>
